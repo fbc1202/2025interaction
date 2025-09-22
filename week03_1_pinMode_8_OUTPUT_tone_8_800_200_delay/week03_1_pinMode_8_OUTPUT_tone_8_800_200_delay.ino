049430ef938bc529743821f498cc36c434906755
fbc1202 <112918680+fbc1202@users.noreply.github.com>
2025-09-22 16:24:14 +0800

    week03

---
+++ week03_1_pinMode_8_OUTPUT_tone_8_800_200_delay/week03_1_pinMode_8_OUTPUT_tone_8_800_200_delay.ino
@@ -0,0 +1,16 @@
+void setup() {
+  // put your setup code here, to run once:
+
+  pinMode(8, OUTPUT);//負責發聲
+  tone(8, 800, 200);
+  delay(1000);
+  tone(8, 800, 200);
+  delay(1000);
+  tone(8, 800, 200);
+  delay(1000);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
